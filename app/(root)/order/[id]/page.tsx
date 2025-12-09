e7f30e8d018affe0d3b3e4c2705498be08427f21
Ahmed Fared <ahmed.super997@gmail.com>
2025-12-09 06:02:38 +0200

    feat(order): Add order management features

---
+++ app/(root)/order/[id]/page.tsx
@@ -0,0 +1,28 @@
+import { getOrderById } from "@/lib/actions/order.actions";
+import { notFound } from "next/navigation";
+import { SearchParamProps, ShippingAddress } from "@/types";
+import OrderDetailsTable from "./_components/order-details-table";
+import { auth } from "@/auth";
+
+export const metadata = {
+  title: "Order Details",
+};
+
+const OrderDetailsPage = async ({ params }: SearchParamProps) => {
+  const { id } = await params;
+  const order = await getOrderById(id);
+  if (!order) notFound();
+  const session = await auth();
+  return (
+    <OrderDetailsTable
+      order={{
+        ...order,
+        shippingAddress: order.shippingAddress as ShippingAddress,
+      }}
+      paypalClientId={process.env.PAYPAL_CLIENT_ID || "sb"}
+      isAdmin={session?.user.role === "admin"}
+    />
+  );
+};
+
+export default OrderDetailsPage;
