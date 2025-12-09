9e3975562f771731816034a25d34b0cdf7ec1169
Ahmed Fared <ahmed.super997@gmail.com>
2025-12-09 06:04:01 +0200

    chore(deps): Update dependencies and configurations

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,45 @@
-declare type SearchParamProps = {
+import {
+  cartItemSchema,
+  insertCartSchema,
+  insertOrderItemSchema,
+  insertOrderSchema,
+  insertProductSchema,
+  paymentResultSchema,
+  shippingAddressSchema,
+} from "@/lib/validator";
+import z from "zod";
+
+export type SearchParamProps = {
   params: Promise<{ [key: string]: string }>;
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+  searchParams: Promise<{ [key: string]: string | undefined }>;
 };
+
+export type userRole = "admin" | "user";
+
+export type Product = z.infer<typeof insertProductSchema> & {
+  id: string;
+  createdAt: Date;
+  rating: string;
+  numReviews: number;
+};
+
+export type Cart = z.infer<typeof insertCartSchema>;
+
+export type CartItem = z.infer<typeof cartItemSchema>;
+
+export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
+
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;
+
+export type Order = z.infer<typeof insertOrderSchema> & {
+  id: string;
+  createdAt: Date;
+  isPaid: boolean;
+  paidAt: Date | null;
+  isDelivered: boolean;
+  deliveredAt: Date | null;
+  orderItems: OrderItem[];
+  user: { name: string; email: string };
+};
+
+export type PaymentResult = z.infer<typeof paymentResultSchema>;
