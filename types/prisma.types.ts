0958468be6cd10ff94f0fa8b2049513990fa5650
Ahmed Fared <ahmed.super997@gmail.com>
2025-12-09 06:04:35 +0200

    refactor: Refactor and reorganize code

--- types/prisma.types.ts
+++
@@ -1,8 +0,0 @@
-import { Decimal } from "@prisma/client/runtime/library";
-import { Product as ProductPrisma } from "@prisma/client";
-
-export type ReplaceDecimalWithString<T> = {
-  [K in keyof T]: T[K] extends Decimal ? string : T[K];
-};
-
-export type Product = ReplaceDecimalWithString<ProductPrisma>;
