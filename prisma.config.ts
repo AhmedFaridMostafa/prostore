b0652e3ebebd317f63657c57fbd8f6af736560ca
Ahmed Fared <ahmed.super997@gmail.com>
2025-12-19 23:34:40 +0200

    feat: Add review model and database migration

---
+++ prisma.config.ts
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import type { PrismaConfig } from "prisma";
+import { defineConfig, env } from "prisma/config";
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+}) satisfies PrismaConfig;
