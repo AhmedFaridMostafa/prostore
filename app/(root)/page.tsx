76f9f5e61ec8448b231525453cbf8fb68b8ed763
Ahmed Fared <ahmed.super997@gmail.com>
2025-12-27 05:36:02 +0200

    Integrate IconBoxes and DealCountdown into home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,18 +6,22 @@ import ProductList from "@/components/shared/product/product-list";
 import { Product } from "@/types";
 import ProductCarousel from "@/components/shared/product/product-carousel";
 import ViewAllProductsButton from "@/components/view-all-products-button";
+import IconBoxes from "@/components/icon-boxes";
+import DealCountdown from "@/components/deal-countdown";
 
 const Home = async () => {
   const latestProducts = (await getLatestProducts()) as unknown as Product[];
   const featuredProducts = await getFeaturedProducts();
   return (
-    <div>
+    <>
       {featuredProducts.length > 0 && (
         <ProductCarousel data={featuredProducts} />
       )}
       <ProductList title="Newest Arrivals" data={latestProducts} />
       <ViewAllProductsButton />
-    </div>
+      <IconBoxes />
+      <DealCountdown />
+    </>
   );
 };
 
